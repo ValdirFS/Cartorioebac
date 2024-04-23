4f3db47ec85e83dee2ec0a3bc82dbd4ee051b815
ValdirFS <167816616+ValdirFS@users.noreply.github.com>
2024-04-22 21:16:04 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- Cartorio.c
+++ Cartorio.c
@@ -129,8 +129,10 @@ int main()
 	    printf("Escolha a opção desejada no menu:\n\n");
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
-	    printf("\t3 - Deletar nomes\n\n"); 
+	    printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n"); 
 	    printf("Opcao: ");//Final do menu
+	    
 	
 	    scanf("%d", &opcao); //Armazenado a escolha do usuário
 	
@@ -148,6 +150,11 @@ int main()
 		
 		    case 3:
 		    deletar();
+		    break;
+		    
+		     case 4:
+		    printf("Obrigado por utilizar o sistema!\n\n");
+		    return 0;
 		    break;
 		
 		    default:	
